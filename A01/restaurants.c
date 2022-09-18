db43c4de9e33ddae8d4e55976cd60a12fb3f7a8a
Dylan Soemitro <dylansoemitro@gmail.com>
2022-09-18 08:40:00 -0400

    restaurants

--- A01/restaurants.c
+++ A01/restaurants.c
@@ -7,35 +7,71 @@ struct restaurant{
   int openHour;
   int closeHour;
   float stars;
-}
+};
+
 int main() {
+
+  struct restaurant restaurant_arr[3];
+  //struct restaurant restaurant1, restaurant2, restaurant3;
+
+  strcpy(restaurant_arr[0].name, "Sushi Atsumi");
+  restaurant_arr[0].openHour = 11;
+  restaurant_arr[0].closeHour = 23;
+  restaurant_arr[0].stars = 3.5;
+
+  strcpy(restaurant_arr[1].name, "The Thirsty Horse");
+  restaurant_arr[1].openHour = 17;
+  restaurant_arr[1].closeHour = 2;
+  restaurant_arr[1].stars = 4.5;
+
+  strcpy(restaurant_arr[2].name, "Magic Bean Coffee");
+  restaurant_arr[2].openHour = 6;
+  restaurant_arr[2].closeHour = 15;
+  restaurant_arr[2].stars = 4.1;
+  int hour;
+  printf("Welcome to Steven Struct's Restaurant List. \nWhat hour is it (24 hour clock)?  ");
+  scanf("%d", &hour);
   
 
-  char word[1024];
- 
-
-  printf("Enter a word: ");
-  scanf("%s", word);
-  int shift;
-  printf("Enter a cypher: ");
-  scanf("%d", &shift);
-  
-  int len = strlen(word);
   //loop through the string
-  for (int i = 0; i < len; i++) {
-    int ascii = word[i];
-    //use ascii modulation 'a'=97
-    int encrypted = (shift+ascii-97)%26+97;
-    //check if negative shift results in ascii code before a,
-    //meaning that we need to loop back around the alphabet
-    if(encrypted<97){
-       word[i] = encrypted+26;
+  for (int i = 0; i < 3; i++) {
+    printf("%d) %s open: %02d:00 close: %02d:00 stars: %0.1f\n", i, restaurant_arr[i].name, restaurant_arr[i].openHour, restaurant_arr[i].closeHour, restaurant_arr[i].stars);
+  }
+
+  int restaurantToVisit;
+  printf("What restaurant do you want to visit ? [0,1,2] ");
+  scanf("%d", &restaurantToVisit);
+
+  if (hour == restaurant_arr[restaurantToVisit].closeHour){
+     printf("Sorry, %s is closing!", restaurant_arr[restaurantToVisit].name);
+  }
+  else if (restaurant_arr[restaurantToVisit].closeHour > restaurant_arr[restaurantToVisit].openHour){
+      if(hour < restaurant_arr[restaurantToVisit].closeHour && hour > restaurant_arr[restaurantToVisit].openHour){
+        printf("Excellent choice. %s will be open for %d more hours.\n", restaurant_arr[restaurantToVisit].name, restaurant_arr[restaurantToVisit].closeHour-hour);
+      }
+      else if(hour > restaurant_arr[restaurantToVisit].closeHour){
+        printf("%s is closed at %d o'clock!!\n", restaurant_arr[restaurantToVisit].name, restaurant_arr[restaurantToVisit].closeHour);
+      }
+      else{        
+        printf("%s isn't open until %d o'clock!!\n", restaurant_arr[restaurantToVisit].name, restaurant_arr[restaurantToVisit].openHour);
+      }
+  }
+  else{
+    if(hour<24 && hour>restaurant_arr[restaurantToVisit].openHour){
+      printf("Excellent choice. %s will be open for %d more hours.\n", restaurant_arr[restaurantToVisit].name, 24-hour);
+    } 
+    else if(hour<restaurant_arr[restaurantToVisit].closeHour){
+      printf("Excellent choice. %s will be open for %d more hours.\n", restaurant_arr[restaurantToVisit].name, restaurant_arr[restaurantToVisit].closeHour-hour);
+    }
+    else if(hour > restaurant_arr[restaurantToVisit].closeHour){
+      printf("%s is closed at %d o'clock!!\n", restaurant_arr[restaurantToVisit].name, restaurant_arr[restaurantToVisit].closeHour);
     }
     else{
-      word[i] = encrypted;
+      printf("%s isn't open until %d o'clock!!\n", restaurant_arr[restaurantToVisit].name, restaurant_arr[restaurantToVisit].openHour);
+
     }
   }
-   printf("Your cypher is %s \n", word);
+
+  
   return 0;
 }
-  22   vbv
