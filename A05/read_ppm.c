6b0b87b63fdc7814532756efbb78b5f4fe3eb270
dylansoemitro <44148714+dylansoemitro@users.noreply.github.com>
2022-10-18 17:25:16 -0400

    write_ppm

--- A05/read_ppm.c
+++ A05/read_ppm.c
@@ -3,12 +3,59 @@
 #include <string.h>
 #include "read_ppm.h"
 
-// implement one
+// Choose *one* to implement
+// struct ppm_pixel {
+//     unsigned char red;
+//     unsigned char green;
+//     unsigned char blue;
+// };
 
-struct ppm_pixel* read_ppm(const char* filename, int* w, int* h) {
-  return NULL;
+
+struct ppm_pixel* read_ppm(const char* filename, int* width, int* height) {
+  FILE* infile = NULL;
+  infile = fopen(filename, "r");
+  if (infile == NULL) {
+    printf("Error opening file. %s\n", filename);
+    return NULL;
+  }
+  //scan through header
+  char magic[128];
+  fgets(magic, sizeof(magic),infile);
+  if (strcmp(magic, "P6\n") != 0) {
+    printf("Error: magic number is not P6.\n");
+    exit(1);
+  }
+  char comments[128];
+  
+  while(fgets(comments, sizeof(comments),infile)[0] == '#') {
+    continue;
+  }
+  //read width and height
+  sscanf(comments, "%d %d", width, height);
+  int maxval;
+  fscanf(infile, "%d", &maxval);
+  //skip last whitespace char
+  fgets(comments, sizeof(comments),infile);
+  //allocate memory for image
+  struct ppm_pixel* image = malloc(*width * *height * sizeof(struct ppm_pixel));
+  if (image == NULL) {
+    printf("Error: memory allocation failed.");
+    return NULL;
+  }
+
+  //read in image
+  printf("Testing file %s : %d %d\n", filename, *width, *height);
+  for(int i = 0; i < *width * *height; i++) {
+    fread(&image[i].red, sizeof(unsigned char), 1, infile);
+    fread(&image[i].green, sizeof(unsigned char), 1, infile);
+    fread(&image[i].blue, sizeof(unsigned char), 1, infile);
+  }
+  fclose(infile);
+  return image;
+
+
 }
 
-struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
-  return NULL;
-}
+// struct ppm_pixel** read_ppm_2d(const char* filename, int* w, int* h) {
+//   return NULL;
+// }
--- A05/write_ppm.c
+++ A05/write_ppm.c
@@ -5,7 +5,21 @@
 // implement one
 
 void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
-
+    FILE* outfile = NULL;
+    outfile = fopen(filename, "w");
+    if (outfile == NULL) {
+        printf("Error opening file. %s\n", filename);
+        return;
+    }
+    fprintf(outfile, "P6\n");
+    fprintf(outfile, "%d %d\n", w, h);
+    fprintf(outfile, "255\n");
+    for(int i = 0; i < w * h; i++) {
+        fwrite(&pixels[i].red, sizeof(unsigned char), 1, outfile);
+        fwrite(&pixels[i].green, sizeof(unsigned char), 1, outfile);
+        fwrite(&pixels[i].blue, sizeof(unsigned char), 1, outfile);
+    }
+    fclose(outfile);
 }
 
 void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) {
