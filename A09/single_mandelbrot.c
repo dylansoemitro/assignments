f9c85e4fa6274fd85c3d89c6972098f4b1231a30
Dylan Raditya Soemitro <dsoemitro1@goldengate.cs.brynmawr.edu>
2022-11-28 14:13:26 -0500

    single_mandelbrot

--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -6,6 +6,8 @@
 #include "read_ppm.h"
 #include "write_ppm.h"
 
+
+
 int main(int argc, char* argv[]) {
   int size = 480;
   float xmin = -2.0;
@@ -32,7 +34,60 @@ int main(int argc, char* argv[]) {
   // todo: your work here
   // generate pallet
   srand(time(0));
+  struct ppm_pixel* pallet = malloc (sizeof(struct ppm_pixel) * maxIterations);
+  for (int i = 0; i < maxIterations; i++) {
+    pallet[i].red = rand() % 256;
+    pallet[i].green = rand() % 256;
+    pallet[i].blue = rand() % 256;
+  }
 
-  // compute image
+  // generate pixels
+  struct ppm_pixel* pixels = malloc (sizeof(struct ppm_pixel) * size * size);
+  
+  //mandlebrot
+  struct timeval tstart, tend;
+  double timer;
+  gettimeofday(&tstart, NULL);
+  for(int r = 0; r < size; r++) {
+    for(int c = 0; c < size; c++) {
+      float xfrac = (float) c / (float) size;
+      float yfrac = (float) r / (float) size;
+      float x_0 = xmin + xfrac * (xmax - xmin);
+      float y_0 = ymin + yfrac * (ymax - ymin);
+
+      float x = 0.0;
+      float y = 0.0;
+      int iteration = 0;
+
+      while(iteration<maxIterations && x*x + y*y < 2*2) {
+        float xtemp = x*x - y*y + x_0;
+        y = 2*x*y + y_0;
+        x = xtemp;
+        iteration++;
+      }
+      //escaped
+      if(iteration<maxIterations) {
+        pixels[r * size + c] = pallet[iteration];
+      }
+      else{
+        pixels[r * size + c].red = 0;
+        pixels[r * size + c].green = 0;
+        pixels[r * size + c].blue = 0;
+      }
+    }
+  }
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  int timestamp = time(0);
+  printf("Computed mandelbrot set (size=%dx%d) in %f seconds\n", size, size, timer);
+
+  // write ppm
+  char filename[256];
+  sprintf(filename, "mandelbrot_%d_%d.ppm", size, timestamp);
+  write_ppm(filename, pixels, size, size);
+  free(pixels);
+  free(pallet);
+
+  return 0;
 
 }
