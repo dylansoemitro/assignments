5847eba3ad21a7e81743ca2c5b0b4e834b7bcf4c
Dylan Raditya Soemitro <dsoemitro1@goldengate.cs.brynmawr.edu>
2022-12-13 02:06:43 -0500

    A09

--- A09/multi_mandelbrot.c
+++ A09/multi_mandelbrot.c
@@ -35,8 +35,86 @@ int main(int argc, char* argv[]) {
   printf("  Num processes = %d\n", numProcesses);
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
-
   // todo: your code here
   // generate pallet
+  srand(time(0));
+  //construct mandelbrot set using 4 child processes
+  pid_t pid;
+  int status;
+  int shm_id;
+
+
+  struct ppm_pixel* pallet = malloc (sizeof(struct ppm_pixel) * maxIterations);
+  for (int i = 0; i < maxIterations; i++) {
+    pallet[i].red = rand() % 256;
+    pallet[i].green = rand() % 256;
+    pallet[i].blue = rand() % 256;
+  }
+  //mandlebrot
+  
+  int shmid;
+  shmid = shmget(IPC_PRIVATE, sizeof(struct ppm_pixel) * size * size, IPC_CREAT | 0666);
+  if (shmid == -1){
+    perror("Error: cannot initialize shared memory\n");
+    exit(1);
+  }
+  struct ppm_pixel* shm_pixels = shmat(shmid, NULL, 0);
+  if (shm_pixels == (void*)-1){
+    perror("Error: cannot access shared memory\n");
+    exit(1);
+  }
+  //Your program should use shared memory to split the computation of the image across 4 child processes. After each child completes, the parent process should save the result to a file using write_ppm.
+  //process 1 row 0-240, col 0-240, process 2 row 0-240, col 240-480, process 3 row 240-480, col 0-240, process 4 row 240-480, col 240-480
+  struct timeval tstart, tend;
+  double timer;
+  gettimeofday(&tstart, NULL);
+  for(int i = 0; i < numProcesses; i++){
+    pid = fork();
+    if(pid == 0){
+      //child process
+      printf("Launched child process: %d\n", getpid());
+      int startRow = (i / 2) * (size / 2);
+      int endRow = startRow + (size / 2);
+      int startCol = (i % 2) * (size / 2);
+      int endCol = startCol + (size / 2);
+      printf("%d) Sub-image block: cols (%d,%d) to rows (%d,%d)\n", getpid(), startRow, endRow, startCol, endCol);
+      for (int row = startRow; row < endRow; row++) {
+        for (int col = startCol; col < endCol; col++) {
+          float x0 = xmin + col * (xmax - xmin) / size;
+          float y0 = ymin + row * (ymax - ymin) / size;
+          float x = 0.0;
+          float y = 0.0;
+          int iteration = 0;
+          while (x*x + y*y < 2*2 && iteration < maxIterations) {
+            float xtemp = x*x - y*y + x0;
+            y = 2*x*y + y0;
+            x = xtemp;
+            iteration++;
+          }
+          shm_pixels[row * size + col] = pallet[iteration];
+        }
+      }
+      exit(0);
+    }
+  }
+  for(int i = 0; i < numProcesses; i++){
+    pid = wait(&status);
+    printf("Child process complete: %d\n", getpid());
+  }
+
+
+  gettimeofday(&tend, NULL);
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  int timestamp = time(0);
+  printf("Computed mandelbrot set (size=%dx%d) in %f seconds\n", size, size, timer);
+
+  // write ppm
+  char filename[256];
+  sprintf(filename, "multi-mandelbrot_%d_%d.ppm", size, timestamp);
+  write_ppm(filename, shm_pixels, size, size);
+  free(pallet);
+  
+  return 0;
+  
   // compute image
 }
